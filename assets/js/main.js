01facd39288e25fe0851d470239fa4556ec81f7c
Lucas Borim <lucas_7005@hotmail.com>
2018-02-20 12:38:16 -0300

    Uplado teste 2

---
+++ assets/js/main.js
@@ -0,0 +1,80 @@
+
+//Utiliza a mascara de CEP no campo de cep.
+    $(document).ready(function(){
+        $('#cep').mask('00.000-000');
+    });
+
+// Script Viacep
+
+$(document).ready(function() {
+
+    function limpa_formulário_cep() {
+        // Limpa valores do formulário de cep.
+        $("#endereco").val("");
+        $("#bairro").val("");
+        $("#cidade").val("");
+        $("#uf").val("");
+    }
+    
+    //Quando o campo cep perde o foco.
+    $("#cep").blur(function() {
+
+        //Nova variável "cep" somente com dígitos.
+        var cep = $(this).val().replace(/\D/g, '');
+
+        //Verifica se campo cep possui valor informado.
+        if (cep != "") {
+
+            //Expressão regular para validar o CEP.
+            var validacep = /^[0-9]{8}$/;
+
+            //Valida o formato do CEP.
+            if(validacep.test(cep)) {
+
+                //Preenche os campos com "..." enquanto consulta webservice.
+                $("#rua").val("...");
+                $("#bairro").val("...");
+                $("#cidade").val("...");
+                $("#uf").val("...");
+                $("#ibge").val("...");
+
+                //Consulta o webservice viacep.com.br/
+                $.getJSON("https://viacep.com.br/ws/"+ cep +"/json/?callback=?", function(dados) {
+
+                    if (!("erro" in dados)) {
+                        //Atualiza os campos com os valores da consulta.
+                        $("#endereco").val(dados.logradouro);
+                        $("#bairro").val(dados.bairro);
+                        $("#cidade").val(dados.localidade);
+                        $("#uf").val(dados.uf);
+
+                        //Desabilita o campo para inclusão manual
+                        $('#endereco').prop('disabled', true);
+                        $('#bairro').prop('disabled', true);
+                        $('#cidade').prop('disabled', true);
+                        $('#uf').prop('disabled', true);
+                    } //end if.
+                    else {
+                        //CEP pesquisado não foi encontrado.
+                        limpa_formulário_cep();
+                        alert("CEP não encontrado, insira o endereço manualmente");
+                        //habilita o campo para inclusão manual
+                        $('#endereco').prop('disabled', false);
+                        $('#bairro').prop('disabled', false);
+                        $('#cidade').prop('disabled', false);
+                        $('#uf').prop('disabled', false);
+                    }
+                });
+            } //end if.
+            else {
+                //cep é inválido.
+                limpa_formulário_cep();
+                alert("Formato de CEP inválido.");
+            }
+        } //end if.
+        else {
+            //cep sem valor, limpa formulário.
+            limpa_formulário_cep();
+        }
+    });
+});
